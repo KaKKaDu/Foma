b6d447aa5ef96695c054225b5471a56cbda4644b
KaKKaDu <kirrakorrt@gmail.com>
2023-02-24 17:17:27 +0300

    almost done

--- js/foma.js
+++ js/foma.js
@@ -1,29 +1,7 @@
 
-
-window.currentopt = 'main';
-
-$('#primarynavbar').on('activate.bs.scrollspy', function () {
-	var hash = $(this).find(".nav-item .nav-link.active");
-	hash.parentElement.classList.add('.nav-item-click');
-	console.log(hash);
-})
-
-document.addEventListener('click', function (point) {
-	if (point.target.classList.contains('nav-item')) {
-		console.log(window.currentopt);
-		console.log(document.getElementById(window.currentopt));
-		document.getElementById(window.currentopt).classList.remove('nav-item-click');
-		let tar = point.target;
-		tar.classList.add('nav-item-click');
-		window.currentopt = point.target.id;
-		console.log(window.currentopt);
-	} else if (point.target.classList.contains('nav-link')) {
-		console.log(window.currentopt);
-		console.log(document.getElementById(window.currentopt));
-		document.getElementById(window.currentopt).classList.remove('nav-item-click');
-		let tar = point.target.parentElement;
-		tar.classList.add('nav-item-click');
-		window.currentopt = point.target.parentElement.id;
-		console.log(window.currentopt);
+let foot = $('#footer');
+$(window).scroll(function() {
+	if ($(window).scrollTop == $(document).height() - $(window).height()) {
+		$('#footer').addClass('nav-link.active');
 	}
 })
